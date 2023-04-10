315f135f25efc1e4d2a69807d92deda82dd59757
LegenDUST <dhnam0502@naver.com>
2023-04-10 14:03:54 +0000

    Lint tag system

--- tag_system.py
+++ tag_system.py
@@ -4,14 +4,16 @@ TagSysSymbol = str
 TagSysSymbolSet = set[TagSysSymbol]
 TagSysTransition = dict[TagSysSymbol, list[TagSysSymbol]]
 
+
 class TagSysTape:
     """Tape for Tag System
     """
-    def __init__(self, tape: list[TagSysSymbol]|None = None):
+
+    def __init__(self, tape: list[TagSysSymbol] | None = None):
         self.tape = []
         if tape is not None:
             self.tape = tape
-            
+
     @property
     def head(self) -> TagSysSymbol:
         """First element of the tape.
@@ -20,7 +22,7 @@ class TagSysTape:
             TagSysSymbol: Head element
         """
         return self.tape[0]
-    
+
     def remove(self, num: int):
         """Remove first `num` entries from tape
 
@@ -28,7 +30,7 @@ class TagSysTape:
             num (int): Number of entries to remove
         """
         self.tape = self.tape[num:]
-        
+
     def tag(self, symbols: list[TagSysSymbol]):
         """Tag the tape with given symbols
 
@@ -36,28 +38,29 @@ class TagSysTape:
             symbols (list[TagSysSymbol]): Symbols to tag with
         """
         self.tape.extend(symbols)
-        
+
     def __str__(self):
         return str(self.tape)
-        
+
 
 class TagSystem:
     """Tag System
     """
+
     def __init__(
-            self,
-            transition: TagSysTransition,
-            num:int = 2,
-            tape: TagSysTape|list[TagSysSymbol]|None = None,
-        ):
-        self.tape:TagSysTape
+        self,
+        transition: TagSysTransition,
+        num: int = 2,
+        tape: TagSysTape | list[TagSysSymbol] | None = None,
+    ):
+        self.tape: TagSysTape
         self.transition = transition
         if isinstance(tape, TagSysTape):
             self.tape = tape
         else:
             self.tape = TagSysTape(tape)
         self.num = num
-        
+
     @property
     def symbol_set(self) -> TagSysSymbolSet:
         """Symbol set of the system. Read only. Based on transition table.
@@ -66,7 +69,7 @@ class TagSystem:
             TagSysSymbolSet: Set of symbols
         """
         return set(self.transition)
-    
+
     def __next__(self):
         try:
             self.tape.tag(self.transition[self.tape.head])
@@ -74,23 +77,24 @@ class TagSystem:
             raise StopIteration from err
         self.tape.remove(self.num)
         return self
-        
+
     def __iter__(self):
         return self
-    
+
     def __str__(self):
         return str(self.tape)
-        
+
+
 if __name__ == "__main__":
     transition_table: TagSysTransition = {
         "a": ["b", "c"],
         "b": ["a"],
         "c": ["a", "a", "a", "a"],
     }
-    
+
     tag_machine = TagSystem(transition_table, tape=["a", "a", "a"])
     print(tag_machine)
-    
+
     for _ in range(5):
         next(tag_machine)
         print(tag_machine)
