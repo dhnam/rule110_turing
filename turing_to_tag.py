b6082e24849a64f260dbc5859e746b6e99febcf0
LegenDUST <dhnam0502@naver.com>
2023-04-10 14:59:24 +0000

    Initial for turing to tag

---
+++ turing_to_tag.py
@@ -0,0 +1,120 @@
+"""From turing machien to tag system
+"""
+
+from binary_turing import TuringTransitionTable, TuringStateTransition, TuringTransition,\
+    TuringSymbol, TuringDirection, BinaryTuring, BinaryTuringTape, TuringTapeTuple
+from tag_system import TagSysTransition, TagSysTag, TagSysTape, TagSysSymbol, TagSystem
+
+
+def turing_table_to_tag_transition(turing_table: TuringTransitionTable) -> TagSysTransition:
+    """Make tag transition out of turing transition table
+
+    Args:
+        turing_table (TuringTransitionTable): source
+
+    Returns:
+        TagSysTransition: destination
+    """
+    tag_transition: TagSysTransition = {}
+    for state_k in turing_table:
+        transition = turing_table[state_k].transitions
+        h_k: tuple[TagSysTag, TagSysTag] = ([], [])
+        l_k: tuple[TagSysTag, TagSysTag] = ([], [])
+        r_k: tuple[TagSysTag, TagSysTag] = ([], [])
+        for z in (0, 1):
+            if transition[z].move == TuringDirection.LEFT and transition[z].write == TuringSymbol.ONE:
+                h_k[z].append(f"R_{transition[z].next_state}*")
+                h_k[z].append(f"R_{transition[z].next_state}*")
+            if z == 0:
+                h_k[z].append(f"H_{transition[z].next_state}")
+            h_k[z].append(f"H_{transition[z].next_state}")
+            if transition[z].move == TuringDirection.RIGHT and transition[z].write == TuringSymbol.ONE:
+                h_k[z].append(f"L_{transition[z].next_state}")
+                h_k[z].append(f"L_{transition[z].next_state}")
+            tag_transition[f"H_{state_k}:{z}"] = h_k[z]
+
+            l_k[z].append(f"L_{transition[z].next_state}")
+            if transition[z].move == TuringDirection.RIGHT:
+                l_k[z].append(f"L_{transition[z].next_state}")
+                l_k[z].append(f"L_{transition[z].next_state}")
+                l_k[z].append(f"L_{transition[z].next_state}")
+            tag_transition[f"L_{state_k}:{z}"] = l_k[z]
+
+            r_k[z].append(f"R_{transition[z].next_state}")
+            if transition[z].move == TuringDirection.LEFT:
+                r_k[z].append(f"R_{transition[z].next_state}")
+                r_k[z].append(f"R_{transition[z].next_state}")
+                r_k[z].append(f"R_{transition[z].next_state}")
+            tag_transition[f"R_{state_k}:{z}"] = r_k[z]
+        tag_transition[f"R_{state_k}*"] = [f"R_{state_k}", f"R_{state_k}"]
+        tag_transition[f"H_{state_k}"] = [f"H_{state_k}:1", f"H_{state_k}:0"]
+        tag_transition[f"L_{state_k}"] = [f"L_{state_k}:1", f"L_{state_k}:0"]
+        tag_transition[f"R_{state_k}"] = [f"R_{state_k}:1", f"R_{state_k}:0"]
+    return tag_transition
+
+
+def turing_machine_to_tag_tape(turing_machien: BinaryTuring) -> TagSysTape:
+    """Make tag system tape out of current state of turing machine
+
+    Args:
+        turing_machien (BinaryTuring): Source
+
+    Returns:
+        TagSysTape: Destination
+    """
+    tape_state = turing_machien.tape_tuple
+    tag_tape_list: list[TagSysSymbol] = [
+        f"H_{tape_state.state}:0", f"H_{tape_state.state}:1"]
+    for _ in range(tape_state.left_number):
+        tag_tape_list += [f"L_{tape_state.state}:0", f"L_{tape_state.state}:1"]
+    for _ in range(tape_state.right_number):
+        tag_tape_list += [f"R_{tape_state.state}:0", f"R_{tape_state.state}:1"]
+
+    if tape_state.head == TuringSymbol.ZERO:
+        tag_tape_list = tag_tape_list[1:]
+
+    return TagSysTape(tag_tape_list)
+
+def is_step_passed(tag_tape: TagSysTape) -> bool:
+    """Check if one cycle of tag system is passed 
+
+    Args:
+        tag_tape (TagSysTape): tag system to check
+
+    Returns:
+        bool: result
+    """
+    for next_symbol in tag_tape.tape:
+        if next_symbol[-2:] not in (":0", ":1"):
+            return False
+    return True
+
+def tag_tape_to_turing_tape_tuple(tag_tape: TagSysTape) -> TuringTapeTuple:
+    # TODO add docstring
+    assert is_step_passed(tag_tape)
+    tape = tag_tape.tape
+    return TuringTapeTuple(
+        state=int(tape[0][2:-2]),
+        left_number=tape.count(f"L_{tape[0][2:-2]}:0"),
+        right_number=tape.count(f"R_{tape[0][2:-2]}:0"),
+        head=TuringSymbol.ONE if tape[-2:] == ":1" else TuringSymbol.ZERO,
+    )
+    
+
+exemple_turing_table = TuringTransitionTable([TuringStateTransition(
+    0,
+    (
+        TuringTransition(TuringSymbol.ONE, TuringDirection.RIGHT, 0),
+        TuringTransition(TuringSymbol.ZERO, TuringDirection.RIGHT, 0)
+    )
+)])
+
+exemple_tape_state = TuringTapeTuple(
+    state=0,
+    left_number=5,
+    right_number=21,
+    head=TuringSymbol.ONE,
+)
+
+exemple_turing_machien = BinaryTuring(
+    exemple_turing_table, BinaryTuringTape.from_tape_tuple(exemple_tape_state))
